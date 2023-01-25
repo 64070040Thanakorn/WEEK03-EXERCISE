925ec9c048b5a4b8e5cf0008862148e5c01b9b2e
Thanakorn Sriwannawit <64070040@kmitl.ac.th>
2023-01-26 03:21:31 +0700

    my submission

--- exercise-2/answers.js
+++ exercise-2/answers.js
@@ -4,16 +4,38 @@ function convertADtoBE(input) {
     //
     // TODO: แปลงปีใน คริสตศักราช เป็น พุทธศักราช เช่น 2000 เป็น "พ.ศ. 2543"
     // โดยให้เติมตัวอักษร พ.ศ. เข้าไปด้านหน้าด้วย
+    if (input >= 543) {
+        return "พ.ศ. " + parseInt(input + 543)
+    }
+    else return "ไม่ถูกต้อง"
 }
 
 function evenOrOdd(input) {
     // TODO: ให้ตรวจสอบว่าตัวเลข input เป็นเลขคู่หรือเลขคี่
+    if (input % 2 == 0) {
+        return "even"
+    }
+    else return "odd"
 }
 
 function getFullName(input) {
     // TODO: ให้นำคำนำหน้าชื่อ ชื่อต้น นามสกุล มาต่อกัน
+    var sex = input.sex
+    if (sex == "male") {
+        sex = "Mr."
+    }
+    else {
+        sex = "Ms."
+    }
+    return sex + " " + input.firstName + " " + input.lastName
 }
 
 function getFirstName(input) {
     // TODO: ให้ทำการตัดนามสกุลออกโดยใช้ indexOf() และ substring()
+    let text = input
+
+
+    return text.substring(0, text.indexOf(" "))
+    // subtext = "ello wo"
+    // text = "Hello world"
 }
--- exercise-3/answers.js
+++ exercise-3/answers.js
@@ -1,12 +1,15 @@
-function getDayName (input) {
+function getDayName(input) {
     // input เป็นเลข 0-6
     // TODO: แปลงเลข 0-6 เป็น วันอาทิตย์ - วันเสาร์
 
     // hint
     // const daysInWeek = ['วันอาทิตย์', 'วันจันทร์', /*...*/]
+    const daysInWeek = ['วันอาทิตย์', 'วันจันทร์', 'วันอังคาร', 'วันพุธ', 'วันพฤหัสบดี', 'วันศุกร์', 'วันเสาร์']
+
+    return daysInWeek[input]
 }
 
-function formatDate (input) {
+function formatDate(input) {
     // input เป็น Object ในรูปแบบ
     // {
     //     day: 6, // 0=วันอาทิตย์, 1=วันจันทร์, 2=วันอังคาร, ...
@@ -16,9 +19,21 @@ function formatDate (input) {
     // }
     // TODO: แปลง input เป็น String ในรูปแบบ "วันศุกร์ที่ 15 มกราคม พ.ศ. 2564"
 
+    const daysInWeek = ['วันอาทิตย์', 'วันจันทร์', 'วันอังคาร', 'วันพุธ', 'วันพฤหัสบดี', 'วันศุกร์', 'วันเสาร์']
+    const month = ['มกราคม', 'กุมภาพันธ์', 'มีนาคม', 'เมษายน', 'พฤษภาคม', 'มิถุนายน', 'กรกฎาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม', 'พฤศจิกายน', 'ธันวาคม']
+
+    
+    return  daysInWeek[input.day] + "ที่ " + input.date + " " + month[input.month]  + " พ.ศ. " + parseInt(input.year + 543)
+
 }
 
-function findTotal (input) {
+function findTotal(input) {
     // input เป็น array ของตัวเลข
     // TODO: ให้หาผลบวกของเลขทั้งหมดใน input
+    var num = 0;
+    console.log(input.length)
+    for (var i = 0; i < input.length; i++) {
+        num += input[i];
+    }
+    return num
 }
